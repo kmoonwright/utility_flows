68b9f369476283d276405c33c55ec022e6ae1b92
Kyle Moon-Wright <kmoonwright@outlook.com>
2021-04-07 17:10:21 -0700

    Add script to register all relevant flows

---
+++ state_generators/register_all.zsh
@@ -0,0 +1,3 @@
+prefect register --path ./1_random_state_generator.py --project State Generators
+prefect register --path ./2_prev_run_state_changer.py --project State Generators
+prefect register --path ./3_flow_run_generator --project State Generators
