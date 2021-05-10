e7231b39b29bc9a3be26bfcc000e63634a4cffce
Kyle Moon-Wright <kmoonwright@outlook.com>
2021-05-10 01:41:35 -0700

    Add api options

--- azure/0_etl_flow.py
+++ azure/0_etl_flow.py
@@ -1,21 +1,31 @@
 from random import randrange
-from prefect import task, Flow
+from prefect import task, Task, Flow
 
+# TASK DEFINITIONS
+# FUNCTIONAL API
 @task
 def extract():
     return randrange(1, 100)
 
+# IMPERATIVE API
+class Extract(Task):
+    def run(self):
+        # Extract the data
+        return randrange(1, 10)
+
 @task
-def transform(data):
-    return data * 10
+def transform(num1, num2):
+    return (num1 + num2) * 10
 
 @task
 def load(data):
     print(f"\nHere's your data: {data}")
 
+# FLOW DEFINITION
 with Flow("My Event Triggered ETL") as flow:
-    e = extract()
-    t = transform(e)
+    extracted_data_1 = extract()
+    extracted_data_2 = Extract()
+    t = transform(extracted_data_1, extracted_data_2)
     l = load(t)
 
 if __name__ == "__main__":
