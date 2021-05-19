67e10e8209c9094ff9786e2140b28f3c0698a5d6
Kyle Moon-Wright <kmoonwright@outlook.com>
2021-05-19 16:17:34 -0700

    Add filtered statements, flow storage, registration destination, and additional imports

--- logging_demo/log_filter.py
+++ logging_demo/log_filter.py
@@ -1,10 +1,11 @@
+import logging
 import prefect
 from prefect import task, Flow
-import logging
+from prefect.storage import GitHub
 
 class AwesomeFilter(logging.Filter):
     def filter(self, record):
-        filtered = ['sensitive', 'banana']
+        filtered = ['sensitive', 'banana', 'papaya']
         if any(x in record.msg for x in filtered):
             return 0
         # you may need to filter based on `getMessage()` if
@@ -33,9 +34,16 @@ def third_task():
     from prefect.engine.signals import FAIL
     raise FAIL(message=my_logger("This is sensitive data"))
 
-with Flow("test") as flow:
+with Flow(
+    "Filtered Logging Demo",
+    storage=GitHub(
+        repo="kmoonwright/utility_flows",
+        path="logging_demo/log_filter.py",
+        access_token_secret="GITHUB_ACCESS_TOKEN"
+    )
+) as flow:
     first_task()
-    second_task()
-    third_task()
+    second_task(upstream_tasks="first_task")
+    third_task(upstream_tasks="second_task")
 
-flow.run()
+flow.register(project_name="logging-demo")
