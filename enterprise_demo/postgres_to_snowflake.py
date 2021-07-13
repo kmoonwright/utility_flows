057aa501e1019b2c340398da3608fd7ce4fb05b2
Kyle Moon-Wright <kmoonwright@outlook.com>
2021-07-13 10:09:51 -0700

    Add error handling, logger, adjust wait times

--- enterprise_demo/postgres_to_snowflake.py
+++ enterprise_demo/postgres_to_snowflake.py
@@ -1,6 +1,8 @@
 import time
 from datetime import timedelta
+import prefect
 from prefect import task, Flow, Parameter
+from prefect.core import parameter
 from prefect.storage import GitHub
 from prefect.schedules import Schedule
 from prefect.schedules.clocks import IntervalClock
@@ -9,26 +11,30 @@ from prefect.engine import signals
 
 @task
 def connect_to_postgres():
-    time.sleep(10)
+    time.sleep(5)
 
 @task
 def execute_query(client, table_name):
-    time.sleep(30)
-    if table_name is "Users":
+    logger = prefect.context.get("logger")
+    logger.info(f"Table Name: {table_name}")
+    if table_name == "Users":
+        time.sleep(9)
         raise signals.FAIL(message="TableNotFound: The table specified does not exist.")
+    else:
+        time.sleep(19)
     return table_name
 
 @task
 def create_df(data):
-    time.sleep(10)
+    time.sleep(14)
 
 @task
 def connect_to_snowflake():
-    time.sleep(10)
+    time.sleep(4)
 
 @task
 def upload_to_snowflake(client, data):
-    time.sleep(30)
+    time.sleep(14)
 
 with Flow(
     "ETL PostgreSQL to Snowflake",
@@ -37,7 +43,7 @@ with Flow(
         path="enterprise_demo/postgres_to_snowflake.py",
         access_token_secret="GITHUB_ACCESS_TOKEN"
     ),
-    schedule=Schedule(clocks=[IntervalClock(timedelta(minutes=2))]),
+    # schedule=Schedule(clocks=[IntervalClock(timedelta(minutes=2))]),
     run_config=DockerRun(
         labels=["production"],
         image="prefecthq/prefect:latest"
