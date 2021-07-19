049a171247843f5eee50ab0671bc9a89eb0a1b91
Kyle Moon-Wright <kmoonwright@outlook.com>
2021-07-19 15:36:27 -0700

    Add test file import functionality

--- sandbox/call_function_artifact.py
+++ sandbox/call_function_artifact.py
@@ -1,9 +1,9 @@
 import prefect
-from sandbox.test_config import write_error_df_to_artifact
+import test_config as tconfig
 
 @prefect.task
 def do_stuff():
-    write_error_df_to_artifact()
+    tconfig.write_error_df_to_artifact()
 
 with prefect.Flow(
     "Test Flow",
@@ -11,7 +11,20 @@ with prefect.Flow(
         repo="kmoonwright/utility_flows",
         path="sandbox/call_function_artifact.py",
         access_token_secret="GITHUB_ACCESS_TOKEN")
+    # storage=prefect.storage.Docker(
+    #     registry_url="kmoonwright",
+    #     image_name="flows",
+    #     image_tag="artifacts-test",
+    #     files={
+    #     # absolute path source -> destination in image
+    #     "/Users/kyle/projects/utility_flows/sandbox/test_config.py": "/sandbox/test_config.py",
+    # },
+    # env_vars={
+    #     # append modules directory to PYTHONPATH
+    #     "PYTHONPATH": "$PYTHONPATH:sandbox/"
+    # },
 ) as flow:
     do_stuff()
-
-flow.register(project_name="test-flows")
+    
+if __name__ == '__main__':
+    flow.register(project_name="test-flows")
