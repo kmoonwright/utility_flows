8416f2e039be929dd17dae1c2ab4b803e29069ba
Kyle Moon-Wright <kmoonwright@outlook.com>
2021-08-29 01:02:57 -0700

    Refactor and set logging

--- aws/s3_query.py
+++ aws/s3_query.py
@@ -1,36 +1,40 @@
 import utils.s3 as s3
 import datetime
 import pandas as pd
-
+import logging
 import pathlib
+
+logging.basicConfig(level=logging.NOTSET)
 BASE_DIR = pathlib.Path(__file__).parent.resolve()
 
+def main():
+    s3_client = s3.create_client()
 
-s3_client = s3.create_client()
+    s3.list_buckets(s3_client)
 
-s3.list_buckets(s3_client)
+    # s3.upload_to_s3(
+    #     client=s3_client, 
+    #     file_name=f"{BASE_DIR}/test_data.csv", 
+    #     bucket="return-box",
+    #     object_name=f"uploads/test_data.csv"
+    # )
 
-# s3.upload_to_s3(
-#     client=s3_client, 
-#     file_name=f"{BASE_DIR}/test_data.csv", 
-#     bucket="return-box",
-#     object_name=f"uploads/test_data.csv"
-# )
+    # s3.download_from_s3_to_dir(
+    #     s3_client, 
+    #     "return-box", 
+    #     "uploads/test_data.txt"
+    # )
 
-# s3.download_from_s3_to_dir(
-#     s3_client, 
-#     "return-box", 
-#     "uploads/test_data.txt"
-# )
+    # downloaded = s3.download_from_s3_to_memory(
+    #     client=s3_client,
+    #     bucket="return-box",
+    #     file_name="uploads/test_data.csv"
+    # )
+    # # Create dataframe from csv
+    # df = pd.read_csv(downloaded['Body'])
+    # print(df)
 
-# downloaded = s3.download_from_s3_to_memory(
-#     client=s3_client,
-#     bucket="return-box",
-#     file_name="uploads/test_data.csv"
-# )
-# # Create dataframe from csv
-# df = pd.read_csv(downloaded['Body'])
-# print(df)
+    print("\n<<<<DONEZO>>>>")
 
-
-print("\n<<<<DONEZO>>>>")
+if __name__ == "__main__":
+    main()
