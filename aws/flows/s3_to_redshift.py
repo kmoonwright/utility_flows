df664382eb460ff6f55ab3046883c9ef5923f9c5
Kyle Moon-Wright <kmoonwright@outlook.com>
2021-08-30 09:13:04 -0700

    Add annotations and reorganize import statements

--- aws/flows/s3_to_redshift.py
+++ aws/flows/s3_to_redshift.py
@@ -1,13 +1,17 @@
-from prefect import task, Flow, Parameter
-
 import psycopg2
 import pandas as pd
 from pathlib import Path
 import os, sys
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
+from prefect import task, Flow, Parameter
+from prefect.storage import Docker
+from prefect.tasks.notifications.slack_task import SlackTask
+
 import utils.s3 as s3
 import utils.redshift as rs
+
+# ----STAGE 1----
 
 @task
 def connect_to_s3():
@@ -22,31 +26,33 @@ def local_data(file_name):
     file_path = Path(__file__).parent.parent.resolve() / Path(f"data/{file_name}")
     return file_path
 
+# ----STAGE 2----
+
 @task
 def create_dataframe(csv_file):
-    test_df = pd.read_csv(csv_file)
+    return pd.read_csv(csv_file)
 
 @task
 def transform(df):
     return df
 
-print("----STAGE 2.....")
+# ----STAGE 3----
 
 @task
 def connect_to_rs(dbname):
     conn = rs.create_conn(dbname)
     return conn
 
-print("----STAGE 2.....")
-
 @task
 def insert_df(client, df, table):
     rs.insert_df(client, df, table)
     return
 
-print("----STAGE 3.....")
+# ----STAGE 4----
 
+storage = Docker(
 
+)
 
 with Flow("S3 to Redshift") as flow:
     conn = connect_to_s3()
